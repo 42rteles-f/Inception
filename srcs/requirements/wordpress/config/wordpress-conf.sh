200c0bd3a16c9c6949274eee278e0ae91884ff27
Rubens Teles Freire Junior <rteles-f@c2r4s7.42lisboa.com>
2025-03-12 21:49:18 +0000

    save

--- srcs/requirements/wordpress/config/wordpress-conf.sh
+++ srcs/requirements/wordpress/config/wordpress-conf.sh
@@ -19,7 +19,7 @@ if [ ! -f /var/www/wordpress/wp-config.php ]; then
     wp core download --allow-root
     wp config create --dbname=$DATABASE_NAME --dbuser=$DATABASE_USER --dbpass=$DATABASE_PASS --dbhost=$DATABASE_HOST:$DATABASE_PORT --allow-root
     wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-    wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=$WP_U_ROLE --allow-root
+    wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --role=$WP_USER_ROLE --allow-root
 fi
 
 echo "WordPress is installed."
