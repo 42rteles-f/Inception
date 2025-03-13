33d6a9f2bfbde005b26c97d95cca81d499fb6aac
Rubens Teles Freire Junior <rteles-f@c2r3s7.42lisboa.com>
2025-03-13 17:51:04 +0000

    save

--- srcs/requirements/mariadb/config/mariadb-conf.sh
+++ srcs/requirements/mariadb/config/mariadb-conf.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 
 if [ -d "/var/lib/mysql/${DATABASE_NAME}" ]; then
+	echo "enter if $?" >> output.txt
 	exit 0;	
 fi
 
@@ -12,6 +13,6 @@ CREATE USER IF NOT EXISTS '${DATABASE_USER}'@'%' IDENTIFIED BY '${DATABASE_PASS}
 GRANT ALL PRIVILEGES ON ${DATABASE_NAME}.* TO '${DATABASE_USER}'@'%';
 FLUSH PRIVILEGES;"
 
-echo "status $?" > output.txt
+echo "status $?" >> output.txt
 
 service mariadb stop
