27aaf5a87ce3c4ff4fbbc1bf5b1a0e961fc71a24
Rubens Teles Freire Junior <rteles-f@c2r3s7.42lisboa.com>
2025-03-13 18:02:54 +0000

    save

--- srcs/requirements/mariadb/config/mariadb-conf.sh
+++ srcs/requirements/mariadb/config/mariadb-conf.sh
@@ -1,23 +1,16 @@
-#! /usr/bin/env bash
+#!/bin/bash
 
 if [ -d "/var/lib/mysql/${DATABASE_NAME}" ]; then
-	echo "enter if $?" >> output.txt
 	exit 0;	
 fi
 
 service mariadb start
 
-# mariadb -u root -e "
-# CREATE DATABASE IF NOT EXISTS ${DATABASE_NAME};
-# CREATE USER IF NOT EXISTS '${DATABASE_USER}'@'%' IDENTIFIED BY '${DATABASE_PASS}';
-# GRANT ALL PRIVILEGES ON ${DATABASE_NAME}.* TO '${DATABASE_USER}'@'%';
-# FLUSH PRIVILEGES;"
-
-mariadb -u root -e "CREATE DATABASE IF NOT EXISTS ${DATABASE_NAME};"
-mariadb -u root -e "CREATE USER IF NOT EXISTS '${DATABASE_USER}'@'%' IDENTIFIED BY '${DATABASE_PASS}';"
-mariadb -u root -e "GRANT ALL PRIVILEGES ON ${DATABASE_NAME}.* TO '${DATABASE_USER}'@'%';"
-mariadb -u root -e "FLUSH PRIVILEGES;"
-
-echo "status $?" >> output.txt
+mariadb -u root -e "
+	CREATE DATABASE IF NOT EXISTS ${DATABASE_NAME};
+	CREATE USER IF NOT EXISTS '${DATABASE_USER}'@'%' IDENTIFIED BY '${DATABASE_PASS}';
+	GRANT ALL PRIVILEGES ON ${DATABASE_NAME}.* TO '${DATABASE_USER}'@'%';
+	FLUSH PRIVILEGES;
+"
 
 service mariadb stop
