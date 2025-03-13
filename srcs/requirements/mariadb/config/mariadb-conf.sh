905a2c65cd698e8d4ab213bb6297e9218b00ce98
Rubens Teles Freire Junior <rteles-f@c2r3s7.42lisboa.com>
2025-03-13 17:46:46 +0000

    save

--- srcs/requirements/mariadb/config/mariadb-conf.sh
+++ srcs/requirements/mariadb/config/mariadb-conf.sh
@@ -12,10 +12,6 @@ CREATE USER IF NOT EXISTS '${DATABASE_USER}'@'%' IDENTIFIED BY '${DATABASE_PASS}
 GRANT ALL PRIVILEGES ON ${DATABASE_NAME}.* TO '${DATABASE_USER}'@'%';
 FLUSH PRIVILEGES;"
 
-if [ $? -eq 0 ]; then
-    echo "✅ Database and user were successfully created!" > dbanswer
-else
-    echo "❌ Error: Failed to create database/user!" > dbanswer
-fi
+echo "status $?" > output.txt
 
 service mariadb stop
